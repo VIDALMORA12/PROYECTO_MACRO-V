8299d2812654e2cda5c43b8b01ef3eaa41dc1d9e
VIDALMORA12 <antoniohenao10@gmail.com>
2026-02-16 17:56:07 -0500

    se le inserto una diagrama sircular y modificacion en diferentes archivos

--- src/app/pages/ciclo/ciclo.page.ts
+++ src/app/pages/ciclo/ciclo.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-ciclo',
@@ -12,9 +13,54 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 })
 export class CicloPage implements OnInit {
 
+  echartInstance:any;
+  ciclo=[
+
+    {ciclo1:'mm'}
+  ]
+
   constructor() { }
 
   ngOnInit() {
+    this.inicializarEchart();
+  }
+  inicializarEchart() {
+    this.echartInstance = echarts.init(document.getElementById('main'));
+    this.echartInstance.setOption({
+      title: {
+        text: 'Presupuestos por Categoría',
+        subtext: 'Fake Data',
+        left: 'center'
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        orient: 'vertical',
+        left: 'left'
+      },
+      series: [
+        {
+          name: 'Presupuestos',
+          type: 'pie',
+          radius: '50%',
+          data: [
+            { value: 1048, name: 'Search Engine' },
+            { value: 735, name: 'Direct' },
+            { value: 580, name: 'Email' },
+            { value: 484, name: 'Union Ads' },
+            { value: 300, name: 'Video Ads' }
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+          }
+        }
+      ]
+    });
   }
 
 }
