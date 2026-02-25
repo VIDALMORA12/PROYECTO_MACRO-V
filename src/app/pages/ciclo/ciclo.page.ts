ed1097ea8edffc6af4bb4e7fa4e8900e350f208a
angie herreravi <angie02vaherrera@gmail.com>
2026-02-25 11:07:17 -0500

    commit de prueba 2

--- src/app/pages/ciclo/ciclo.page.ts
+++ src/app/pages/ciclo/ciclo.page.ts
@@ -1,75 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-<<<<<<< HEAD
-=======
-import * as echarts from 'echarts';
->>>>>>> origin/main
+export class Ciclo {
 
-@Component({
-  selector: 'app-ciclo',
-  templateUrl: './ciclo.page.html',
-  styleUrls: ['./ciclo.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
-})
-export class CicloPage implements OnInit {
+    private id_ciclo: number;
+    private descripcion: string;
+    private periodicidad: string;
 
-<<<<<<< HEAD
-  constructor() { }
-
-  ngOnInit() {
-=======
-  echartInstance:any;
-  ciclo=[
-
-    {ciclo1:'mm'}
-  ]
-
-  constructor() { }
-
-  ngOnInit() {
-    this.inicializarEchart();
-  }
-  inicializarEchart() {
-    this.echartInstance = echarts.init(document.getElementById('main'));
-    this.echartInstance.setOption({
-      title: {
-        text: 'Presupuestos por Categoría',
-        subtext: 'Fake Data',
-        left: 'center'
-      },
-      tooltip: {
-        trigger: 'item'
-      },
-      legend: {
-        orient: 'vertical',
-        left: 'left'
-      },
-      series: [
-        {
-          name: 'Presupuestos',
-          type: 'pie',
-          radius: '50%',
-          data: [
-            { value: 1048, name: 'Search Engine' },
-            { value: 735, name: 'Direct' },
-            { value: 580, name: 'Email' },
-            { value: 484, name: 'Union Ads' },
-            { value: 300, name: 'Video Ads' }
-          ],
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            }
-          }
-        }
-      ]
-    });
->>>>>>> origin/main
-  }
-
+    constructor(id_ciclo: number, descripcion: string, periodicidad: string) {
+        this.id_ciclo = id_ciclo;
+        this.descripcion = descripcion;
+        this.periodicidad = periodicidad;
+    }
+    getIdCiclo(): number {
+        return this.id_ciclo;
+    }
+    getDescripcion(): string {
+        return this.descripcion;
+    }
+    getPeriodicidad(): string {
+        return this.periodicidad;
+    }
+    setIdCiclo(id_ciclo: number): void {
+        this.id_ciclo = id_ciclo;
+    }
+    setDescripcion(descripcion: string): void {
+        this.descripcion = descripcion;
+    }
+    setPeriodicidad(periodicidad: string): void {
+        this.periodicidad = periodicidad;
+    }
+    
 }
